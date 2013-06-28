0edc115dc1e63f038d1e9ffc41113bfbd63e0e4d
Chris Eldredge <chris.eldredge@gmail.com>
2013-06-28 20:12:27 +0100

    Support relative Uri. Omit blank Uri.

--- source/AspNet.WebApi.HtmlMicrodataFormatter.Tests/UriSerializerTests.cs
+++ source/AspNet.WebApi.HtmlMicrodataFormatter.Tests/UriSerializerTests.cs
@@ -34,5 +34,29 @@ namespace AspNet.WebApi.HtmlMicrodataFormatter.Tests
             Assert.That(attr, Is.Not.Null, "Should include 'itemprop' attribute in <a> element.");
             Assert.That(attr.ToString(), Is.EqualTo(new XAttribute("itemprop", "myProp").ToString()));
         }
+
+        [Test]
+        public void RelativeUri()
+        {
+            var result = (XElement)serializer.Serialize("MyProp", new Uri("/some path?query=y+y#chapter2", UriKind.Relative), context).Single();
+
+            Assert.That(result.Attribute("href").Value, Is.EqualTo("/some%20path?query=y+y#chapter2"));
+        }
+
+        [Test]
+        public void Empty()
+        {
+            var result = serializer.Serialize("MyProp", new Uri("", UriKind.Relative), context);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Null()
+        {
+            var result = serializer.Serialize("MyProp", null, context);
+
+            Assert.That(result, Is.Empty);
+        }
     }
 }
--- source/AspNet.WebApi.HtmlMicrodataFormatter/UriSerializer.cs
+++ source/AspNet.WebApi.HtmlMicrodataFormatter/UriSerializer.cs
@@ -10,15 +10,26 @@ namespace AspNet.WebApi.HtmlMicrodataFormatter
 
         public override IEnumerable<XObject> Serialize(string propertyName, object obj, SerializationContext context)
         {
+            if (obj == null) yield break;
+
             var uri = (Uri) obj;
 
+            var href = uri.IsAbsoluteUri
+                           ? uri.GetComponents(UriComponents.AbsoluteUri, UriFormat.UriEscaped)
+                           : Uri.EscapeUriString(uri.ToString());
+
+            if (string.IsNullOrWhiteSpace(href))
+            {
+                yield break;
+            }
+
             var element = new XElement("a",
-                                       new XAttribute("href", uri.GetComponents(UriComponents.AbsoluteUri, UriFormat.UriEscaped)),
-                                       new XText(uri.GetComponents(UriComponents.AbsoluteUri, UriFormat.Unescaped)));
+                                       new XAttribute("href", href),
+                                       new XText(uri.ToString()));
 
             SetPropertyName(element, propertyName, context);
 
-            return new[] {element};
+            yield return element;
         }
     }
 }
