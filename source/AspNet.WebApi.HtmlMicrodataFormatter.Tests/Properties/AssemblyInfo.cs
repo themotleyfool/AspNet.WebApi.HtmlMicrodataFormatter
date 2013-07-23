aa249335fb7774c3cf9f24a3d576780962b8f90b
Chris Eldredge <chris.eldredge@gmail.com>
2013-07-23 16:32:01 +0100

    Include generated VersionInfo.cs when present.

--- source/AspNet.WebApi.HtmlMicrodataFormatter.Tests/Properties/AssemblyInfo.cs
+++ source/AspNet.WebApi.HtmlMicrodataFormatter.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5daf2b45-c24c-4cd4-ae30-53be8fa53c0c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- source/AspNet.WebApi.HtmlMicrodataFormatter/Properties/AssemblyInfo.cs
+++ source/AspNet.WebApi.HtmlMicrodataFormatter/Properties/AssemblyInfo.cs
@@ -22,17 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7beb923-4883-4ceb-8256-d286ab4c46b5")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
+#if DEBUG
 [assembly: InternalsVisibleTo("AspNet.WebApi.HtmlMicrodataFormatter.Tests")]
+#endif
